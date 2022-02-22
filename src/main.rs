0f2c6d77372cb4a583eaabb7b3d5e21cf6c75851
Meowu <474384902@qq.com>
2022-02-22 23:41:03 +0800

    WIP: try ssh.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::{process::{ Command }, io::Write};
+
 fn main() {
-    println!("Hello, world!");
+    let server = "xxx";
+    let mut child = Command::new("ssh").arg(server).spawn().ok().expect("ssh connect failed.");
+    let _ = {
+        let the_stdid_stream = child.stdin.as_mut().expect("could not get mutable stream.");
+        the_stdid_stream.write(b"xxx");
+        the_stdid_stream.flush();
+    };
+    child.wait();
 }
